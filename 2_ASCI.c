99cfcfebb608bbf5215d56129ba7f3b9c2f6ec1a
tobuso <106862439+tobusoo@users.noreply.github.com>
2022-09-23 21:18:55 +0700

    Add files via upload

---
+++ 2_ASCI.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int i;
+    printf("oct\tdec\thex\tchar\n");
+    for (i = 0; i <= 127; i++){
+        printf("%o\t%d\t%x\t%c\n", i, i, i, i);
+    }
+
+    return 0;
+}
