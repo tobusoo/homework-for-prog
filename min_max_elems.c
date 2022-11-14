072e732d44a602624e29078e020a0f29ef9d3e3b
tobuso <106862439+tobusoo@users.noreply.github.com>
2022-11-14 11:02:33 +0700

    Add files via upload

---
+++ min_max_elems.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void min_max_elems(int array[], int length, int *min, int *max)
+{
+    for (int i = 0; i < length; i++){
+        if (array[i] > *max){
+            *max = array[i];
+        }
+        if (array[i] < *min){
+            *min = array[i];
+        }
+    }  
+}
+
+int main()
+{
+    int length;
+    int max_elems = 0;
+    int min_elems = 100000;
+    int* max = &max_elems;
+    int* min = &min_elems;
+    printf("Введите размер массива: ");
+    scanf("%d", &length);
+    int array[length];
+    printf("Введите %d элементов массива: ", length);  
+    for (int i = 0; i < length; i++){
+        scanf("%d", &array[i]);
+    }
+
+    min_max_elems(array, length, min, max);
+    printf("max elems = %d\nmin elems = %d\n", max_elems, min_elems);
+    return 0;
+}
