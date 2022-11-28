40c4494d2afc2ddb3404d004242c5d2c86123971
tobuso <106862439+tobusoo@users.noreply.github.com>
2022-11-28 21:47:02 +0700

    Add files via upload

--- string_copy_concat.c
+++ string_copy_concat.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #define MAX_SYMBOLS 50
 
 
@@ -22,6 +23,36 @@ void string_concat(char *out, char *src1, char *src2)
 }
 
 
+void string_copy_safe(char *out, char *src, size_t count)
+{
+    while (count-- != 0){
+        *out++ = *src++;
+        if (*src == '\0'){
+            break;
+        }
+    }    
+}
+
+void string_concat_safe(char *out, char *src1, char *src2, size_t count)
+{
+    while (*src1 != '\0'){
+        if (count == 0){
+            break;
+        }
+        count--;
+        *out++ = *src1++;
+    } 
+
+    while (*src2 != '\0'){
+        if (count == 0){
+            break;
+        }
+        count--;
+        *out++ = *src2++;
+    } 
+}
+
+
 int main()
 {
     char string[] = "hello";
@@ -30,15 +61,17 @@ int main()
     char buf_2[MAX_SYMBOLS] = "";
 
     printf("string: \"%s\"\n", string);
-    printf("strin2: \"%s\"\n", string2);
+    printf("string2: \"%s\"\n", string2);
     printf("buffer1: \"%s\"\n", buf_1);
     printf("buffer2: \"%s\"\n", buf_2);
 
-    string_copy(buf_1, string);
-    printf("buffer1 after copy: \"%s\"\n", buf_1);
 
-    string_concat(buf_2, string, string2);
-    printf("buffer2 after concat: \"%s\"\n", buf_2);
+    string_copy_safe(buf_1, string, sizeof(buf_1)-1);
+    printf("buffer1 after copy safe: \"%s\"\n", buf_1);
 
+    string_concat_safe(buf_2, string, string2, sizeof(buf_2)-1);
+    printf("buffer2 after concat safe: \"%s\"\n", buf_2);
+
+    
     return 0;
 }
