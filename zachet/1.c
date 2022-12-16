224c1e9460763db77475388a7093fb63fe0ce148
tobuso <106862439+tobusoo@users.noreply.github.com>
2022-12-16 14:28:39 +0700

    Update 1.c

--- zachet/1.c
+++ zachet/1.c
@@ -8,41 +8,19 @@ void printArray (int array[], int size)
     printf("\n");
 }
 
-int *merge_sort(int *array, int *buffer, int left, int right)
+void selection_sort(int array[], int size)
 {
-    if (left == right){
-        buffer[left] = array[left];
-        return buffer;
-    }
-
-    int middle = left + (right - left) / 2;
-
-    int *l_buff = merge_sort(array, buffer, left, middle);
-    int *r_buff = merge_sort(array, buffer, middle + 1, right);
-
-    int *target = l_buff == array ? buffer : array;
-
-    int l_cur = left, r_cur = middle + 1;
-    
-    for (int i = left; i <= right; i++) {
-        if (l_cur <= middle && r_cur <= right) {
-            if (l_buff[l_cur] < r_buff[r_cur]) { 
-                target[i] = l_buff[l_cur];
-                l_cur++;
-            } else {
-                target[i] = r_buff[r_cur];
-                r_cur++;
+    for (int i = 0; i < size; i++){
+        int min_i = i;
+        for (int j = i; j < size; j++){
+            if (array[j] < array[min_i]) {
+                min_i = j;
             }
         }
-        else if (l_cur <= middle){
-            target[i] = l_buff[l_cur];
-            l_cur++;
-        } else {
-            target[i] = r_buff[r_cur];
-            r_cur++;
-        }
+        int temp = array[i];
+        array[i] = array[min_i];
+        array[min_i] = temp;
     }
-    return target;
 }
 
 int main()
@@ -52,13 +30,13 @@ int main()
     scanf("%d", &size);
 
     int array[size];
-    int buffer[size];
     printf("Enter %d element of array: ", size);
     for (int i = 0; i < size; i++){
         scanf("%d", &array[i]);
     }
+    
+    selection_sort(array, size);    
+    printArray(array, size);
 
-    int *p = merge_sort(array, buffer, 0, size-1);
-    printArray(p, size);
     return 0;
 }
